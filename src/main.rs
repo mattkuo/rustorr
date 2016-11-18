217c9549773ccf03bb403844aa54ee98e8d9a34b
Matthew Kuo <hi@matthewkuo.com>
2016-11-18 02:30:31 -0800

    read option from terminal

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,31 @@
+extern crate getopts;
 extern crate rustorr;
 
+use getopts::Options;
+use std::fs::File;
+use std::env;
+use std::io::Read;
+
 fn main() {
-    let s = "d3:cow3:moo4:spam4:eggse";
-    let iter = s.bytes();
-    let mut hi = rustorr::bencode::bdecoder::Bdecoder::new(iter);
-    println!("{:?}", hi.decode());
+    let args: Vec<String> = env::args().collect();
+    // let program: String = args[0].clone();
+
+    let opts: Options = Options::new();
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => panic!(f.to_string()),
+    };
+
+    let input: String = if !matches.free.is_empty() {
+        matches.free[0].clone()
+    } else {
+        return;
+    };
+
+    let mut string = String::new();
+    File::open(input).unwrap().read_to_string(&mut string).unwrap();
+    
+    let mut decoder = rustorr::bencode::bdecoder::Bdecoder::new(string.bytes());
+    println!("{:?}", decoder.decode());
 }
